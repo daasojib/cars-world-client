901bb4b49b288553a907a518a1c2304b18098a08
dasoib <dasojib49@gmail.comm>
2023-05-20 10:54:39 +0600

    all toys loading

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
 
 const AllToys = () => {
+
+          const [toys, setToys] = useState([])
+
+          useEffect(() => {
+                    fetch('allToys.json')
+                              .then(res => res.json())
+                              .then(data => setToys(data))
+          }, [])
+
           return (
+
                     <div>
-                              <h2>This is all toys</h2>                              
+
+                              <h2 className="font-bold text-2xl">All Toys</h2>
+
+                              <div className="overflow-x-auto">
+                                        {}
+                                        <table className="table w-full">
+                                                  <thead>
+                                                            <tr>
+                                                                      <th>Seller</th>
+                                                                      <th>Toy Name</th>
+                                                                      <th>Sub Category</th>
+                                                                      <th>Price</th>
+                                                                      <th>Quantity</th>
+                                                                      <th></th>
+                                                            </tr>
+                                                  </thead>
+                                                  <tbody>
+                                                            <tr>
+                                                                      <th>{ }</th>
+                                                                      <td>Cy Ganderton</td>
+                                                                      <td>Quality Control Specialist</td>
+                                                                      <td>Blue</td>
+                                                                      <td></td>
+                                                                      <td><button className="btn btn-outline btn-primary">View Details</button></td>
+                                                            </tr>
+                                                  </tbody>
+                                        </table>
+                              </div>
+
                     </div>
           );
 };
